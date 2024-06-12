497b93f5d86ea68e96fddc0699b2daa06208f125
pashaman <plavel@gmail.com>
2024-06-12 21:20:37 +0700

    chore: base project structure and setup

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2020": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 11,
+    "project": "./tsconfig.json"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-constant-condition": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    "@typescript-eslint/no-inferrable-types": "off",
+    "no-useless-escape": "off",
+    "no-array-constructor": "error",
+    "no-empty": ["error", { "allowEmptyCatch": false }]
+  }
+};
---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp accets/swagger-initializer.js node_modules/swagger-ui-dist/swagger-initializer.js;
+chmod -R 777 node_modules/swagger-ui-dist/swagger-initializer.js;
