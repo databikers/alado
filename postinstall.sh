d47980b0e73d24a1e3bb0e7861492bbfe70f86e9
pashaman <plavel@gmail.com>
2024-06-12 21:43:59 +0700

    fix: fixed postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp accets/swagger-initializer.js node_modules/swagger-ui-dist/swagger-initializer.js;
-chmod -R 777 node_modules/swagger-ui-dist/swagger-initializer.js;
+cp accets/swagger-initializer.js ../swagger-ui-dist/swagger-initializer.js;
+chmod -R 777 ../swagger-ui-dist/swagger-initializer.js;
