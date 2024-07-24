3cf5c4baa604368c53a826676ca85c9a45259642
pashaman <plavel@gmail.com>
2024-07-24 14:20:10 +0700

    feat: added constants [default host and backlog]

---
+++ src/constants/http-server.ts
@@ -0,0 +1,2 @@
+export const defaultHost: string = '0.0.0.0';
+export const defaultBacklog: number = 2048;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export * from './content-type';
 export * from './default-doc-object';
 export * from './http-method';
+export * from './http-server';
 export * from './http-status';
 export * from './reg-exp';
