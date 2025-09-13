ae8dd3c1cd93a16e6374ccbc47db290e167c6cbb
pashaman <plavel@gmail.com>
2025-09-13 13:38:41 +0700

    chore: updated export

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,25 @@ export {
   Request,
   Response,
   PropertyDefinition,
+  PropertyDocumentation,
+  PropertyDefinitionSchema,
   FilePropertyDefinition,
 } from '@dto';
 export { AladoServerOptions, ContextOptions, CorsOptions, OpenApiDoc } from '@options';
 export { HttpMethod, httpStatus, ContentType } from '@const';
+export {
+  initializeApplication,
+  defineRequest,
+  defineResponse,
+  fileUpload,
+  documentProperty,
+  validateProperty,
+  withAuth,
+  get,
+  del,
+  head,
+  patch,
+  post,
+  put,
+} from '@decorator';
 export { AladoServer } from '@alado';
